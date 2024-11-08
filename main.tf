fea5d4fc0220921757f3a4d7bbdfb8c4d7a7779f
Yahswanth <140070698+Yahswanth@users.noreply.github.com>
2024-11-08 13:52:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -139,7 +139,7 @@ resource "aws_route_table_association" "b" {
 
 #Create WEB-SERVER-1 Instance
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0984f4b9e98be44bf"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "ebs"
@@ -154,7 +154,7 @@ resource "aws_instance" "webserver1" {
 
 #Create WEB-SERVER-2 Instance
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0984f4b9e98be44bf"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   key_name               = "ebs"
@@ -169,7 +169,7 @@ resource "aws_instance" "webserver2" {
 
 #Create APP SERVER-1 EC2 Instance
 resource "aws_instance" "appserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0984f4b9e98be44bf"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "ebs"
@@ -182,7 +182,7 @@ resource "aws_instance" "appserver1" {
 
 #Create APP SERVER-2 EC2 Instance
 resource "aws_instance" "appserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0984f4b9e98be44bf"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   key_name               = "ebs"
