475b55b2d7b11e68ef6637484d409cd6e6e2d5cc
Yahswanth <140070698+Yahswanth@users.noreply.github.com>
2024-11-08 14:05:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket = "terrastatebyucket00789"
+    bucket = "terrastatebyucket0078"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
