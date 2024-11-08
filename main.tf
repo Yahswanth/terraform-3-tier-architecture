cfd831f26e08be668241783ed1904b5426a3a21d
Yahswanth <140070698+Yahswanth@users.noreply.github.com>
2024-11-08 13:36:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket = "terrastatebyucket00789700"
+    bucket = "terrastatebyucket0078970778"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
@@ -368,7 +368,7 @@ output "lb_dns_name" {
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "pipleinbucket0088bdhuwtrrrrf3t5hd8e82"
+  bucket = "pipleinbucket0088bdhuwtrrrrf3t5hd8e8r"
 }
 
 
